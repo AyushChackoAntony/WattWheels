c28c9bb0d1f3b3ed7a5bb58307b4f7bda66936af
VxPYr <vishnujaswal2004@gmail.com>
2025-08-15 13:21:44 +0530

    Created customer login api routes

--- frontend/app/src/app/login/customer/page.js
+++ frontend/app/src/app/login/customer/page.js
@@ -24,17 +24,48 @@ export default function CustomerLogin() {
       showMessage('Please fill in all fields', 'error');
       return;
     }
+
+    const loginData = {
+      email: email,
+      password: password
+    }
+
     setLoading(true);
-    // Simulate login logic (replace with real API/authManager)
-    setTimeout(() => {
-      setLoading(false);
-      if (email === 'customer@example.com' && password === 'password') {
-        showMessage('Login successful!', 'success');
-        setTimeout(() => router.push('/dashboard/customer'), 1000);
+
+    try {
+      const res = await fetch("/api/customerLogin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(loginData)
+      });
+
+      const data = await res.json();
+      console.log("Response from API route:", data);
+
+      if (res.ok) {
+        showMessage('Login request sent successfully!', 'success');
       } else {
-        showMessage('Invalid credentials', 'error');
+        showMessage(data.error || 'Login failed', 'error');
       }
-    }, 1000);
+    } catch (error) {
+      console.error("Error sending request:", err);
+      showMessage('Something went wrong', 'error');
+    } finally {
+      setLoading(false);
+    }
+    
+    // Simulate login logic (replace with real API/authManager)
+    // setTimeout(() => {
+    //   setLoading(false);
+    //   if (email === 'customer@example.com' && password === 'password') {
+    //     showMessage('Login successful!', 'success');
+    //     setTimeout(() => router.push('/dashboard/customer'), 1000);
+    //   } else {
+    //     showMessage('Invalid credentials', 'error');
+    //   }
+    // }, 1000);
   };
 
   return (
